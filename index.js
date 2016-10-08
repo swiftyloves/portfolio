02878d84236baede7f7d48653d1c921c20abe68f
Japie <jaywiewu@email.com>
2016-10-08 18:18:25 -0400

    Modify listen port

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.get('/website', function(req, res) {
     res.sendFile(path.join(__dirname+'/website.html'));
 });
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
