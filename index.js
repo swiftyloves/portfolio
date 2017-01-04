444478d37dd0557b19454d63b0de1d58419e425a
Japie <jaywiewu@email.com>
2017-01-04 10:45:39 -0500

    ceremony

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.get('/ceremony', function(req, res) {
+    res.sendFile(path.join(__dirname+'/ceremony.html'));
+});
 app.get('/film', function(req, res) {
     res.sendFile(path.join(__dirname+'/film.html'));
 });
@@ -23,6 +26,7 @@ app.get('/game', function(req, res) {
 app.get('/resume', function(req, res) {
     res.sendFile(path.join(__dirname+'/resume.html'));
 });
+
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
